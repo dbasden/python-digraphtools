6cc12be02ba10dd030ade3a5138692e2ba7d3e24
David Basden <davidb-github@rcpt.to>
2011-09-07 15:15:28 +1000

    New release for bracket bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='digraphtools',
-	version='0.1.0',
+	version='0.2.1',
 	author='David Basden',
 	author_email='davidb-python@rcpt.to',
 	packages=['digraphtools','digraphtools.test'],
